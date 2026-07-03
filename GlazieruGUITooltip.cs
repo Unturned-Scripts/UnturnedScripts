007e4f83184907943ee4d2062df837f09af2d9e0
Gamingtoday093 <gamingtoday093@users.noreply.github.com>
2026-07-03 20:33:56 +0000

    03 July 2026 - Version 3.26.3.3 (23686175) [Forced]

---
+++ GlazieruGUITooltip.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SDG.Unturned
+{
+    [AddComponentMenu("Unturned Scripts/Glazieru GUITooltip")]
+    internal class GlazieruGUITooltip : MonoBehaviour
+    {
+        public string text;
+
+        public Color color = Color.white;
+    }
+}
